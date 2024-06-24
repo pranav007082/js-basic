4d12533821393c7682a0bedd0b61d000068d6cbd
pranav007082 <148082845+pranav007082@users.noreply.github.com>
2024-06-24 14:24:46 +0000

    container config done

---
+++ .devcontainer/01_basics/datatype_conversion.js
@@ -0,0 +1,9 @@
+/*
+input - output
+"33"->33 string to number
+"33abc"-> NaN string to number
+1->true ,0->false number to boolean
+""->false "abc"->true string to boolean
+33 -> "33" number to string
+*/
+console.log("do more investigations")
