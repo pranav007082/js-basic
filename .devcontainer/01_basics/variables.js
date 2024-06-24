7d8dfbd6b7a3fa67d36baaf4f3b212ea5bfb1829
pranav007082 <148082845+pranav007082@users.noreply.github.com>
2024-06-24 12:53:17 +0000

    container config done

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,7 @@
+const accid = 123
+let email = "abc@gmail.com"
+city = "hyd"
+let currentstate
+email = "def@gmail.com"
+//prefer not to use var because of functional scope
+console.table([email,accid,city,currentstate])
