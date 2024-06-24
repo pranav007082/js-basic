d4da86d4a2b2f87e73026b4229baced163ce6e9e
pranav007082 <148082845+pranav007082@users.noreply.github.com>
2024-06-24 13:07:33 +0000

    container config done

---
+++ .devcontainer/01_basics/variables_datatypes.js
@@ -0,0 +1,23 @@
+const accid = 123
+let email = "abc@gmail.com"
+city = "hyd"
+let currentstate
+email = "def@gmail.com"
+//prefer not to use var because of functional scope
+console.table([email,accid,city,currentstate])
+
+console.log(typeof(email))
+console.log(typeof(null))
+/*
+datatypes :
+
+string
+number
+bigint
+null ==> standalone value
+boolean
+undefined
+symbol ==> unique
+object
+
+*/
